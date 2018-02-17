b05118f9d637f7ba9530a004131ab48b952b14fa
Manish Kapoor <manish.kapoor@airfi.aero>
2018-02-17 15:05:48 +0530

    Added Spring configuration through through xml file "chuck-norris-config.xml". Configured ChuckNorrisBean.

--- src/main/java/springframework/jokes/JokesApplication.java
+++ src/main/java/springframework/jokes/JokesApplication.java
@@ -3,9 +3,11 @@ package springframework.jokes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 import springframework.jokes.services.JokeService;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-norris-config.xml")
 public class JokesApplication {
 
 	public static void main(String[] args) {
